9f39f616655d460cf8572b3cfd4b59aa613e52a8
Matthew Casperson <matthewcasperson@gmail.com>
2020-08-14 11:33:02 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/AdoptOpenJDK/openjdk14-binaries/releases/download/jdk-14.0.2%2B12/OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz
+tar -xzf OpenJDK14U-jdk_x64_linux_hotspot_14.0.2_12.tar.gz
+mv jdk-14.0.2+12 /opt
